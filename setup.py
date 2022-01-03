6c9f441113be65d70d9844a289e4c461bddc2cef
CashabackLab <97047873+CashabackLab@users.noreply.github.com>
2022-01-03 12:52:31 -0500

    Create setup.py
    First attempt at a setup file, may give warnings

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings 
+    name='DataVisualization',
+    url='https://github.com/CashabackLab/DataVisualization',
+    author='CashabackLab',
+    author_email='cashabacklab@gmail.com',
+    # Needed to actually package something
+    packages=['data_visualization'],
+    # Needed for dependencies
+    install_requires=['numpy', 'matplotlib'],
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='UD',
+    description='Python package for visualizing human kinematic data and analytical results, tailored for the Cashaback Lab',
+    long_description=open('README.txt').read(),
+)
