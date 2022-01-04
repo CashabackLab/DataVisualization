f47bb56627595779d0c0f6ac54cab5a9fcf33587
CashabackLab <97047873+CashabackLab@users.noreply.github.com>
2022-01-04 09:32:38 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     # Needed for dependencies
     install_requires=['numpy', 'matplotlib'],
     # *strongly* suggested for sharing
-    version='0.2',
+    version='0.1',
     # The license can be anything you like
     license='UD',
     description='Python package for visualizing human kinematic data and analytical results, tailored for the Cashaback Lab',
