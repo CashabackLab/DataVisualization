49287c2a1c40272880045e6c373eaf504db5d1ba
CashabackLab <97047873+CashabackLab@users.noreply.github.com>
2022-01-04 09:26:40 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     # Needed to silence warnings 
     name='DataVisualization',
     url='https://github.com/CashabackLab/DataVisualization',
+    download_url = 'https://github.com/CashabackLab/DataVisualization',
     author='CashabackLab',
     author_email='cashabacklab@gmail.com',
     # Needed to actually package something
@@ -11,7 +12,7 @@ setup(
     # Needed for dependencies
     install_requires=['numpy', 'matplotlib'],
     # *strongly* suggested for sharing
-    version='0.2',
+    version='0.1',
     # The license can be anything you like
     license='UD',
     description='Python package for visualizing human kinematic data and analytical results, tailored for the Cashaback Lab',
