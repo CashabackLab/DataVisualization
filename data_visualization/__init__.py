b8d8b67556d7fb2d1b633496a838715d0c42dc1b
Adam Roth <81182782+aroth1338@users.noreply.github.com>
2022-03-03 09:44:43 -0500

    Update __init__.py

--- data_visualization/__init__.py
+++ data_visualization/__init__.py
@@ -1,2 +1,9 @@
-from .plot_functions import *
+#from .plot_functions import * #to be removed next update
 from .color_wheel import *
+from .gen_frame import *
+from .Create_GIF import *
+from .set_Axes_Color import *
+from .plot_inset_image import *
+from .Custom_Legend import *
+from .Pair_Plot import *
+from .Stat_Annotation import *
