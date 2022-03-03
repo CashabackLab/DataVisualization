6a1f49e372d6dfbe8b84a0b35160d6c6440be6c1
Adam Roth <81182782+aroth1338@users.noreply.github.com>
2022-03-03 09:33:03 -0500

    Create gen_fram.py

---
+++ data_visualization/gen_fram.py
@@ -0,0 +1,25 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from PIL import Image
+
+def gen_frame(path):
+    """
+    Input: path to image
+    Generates the single frame of a gif from an image
+    """
+    im = Image.open(path)
+    alpha = im.getchannel('A')
+
+    # Convert the image into P mode but only use 255 colors in the palette out of 256
+    im = im.convert('RGB').convert('P', palette=Image.ADAPTIVE, colors=255)
+
+    # Set all pixel values below 128 to 255 , and the rest to 0
+    mask = Image.eval(alpha, lambda a: 255 if a <=128 else 0)
+
+    # Paste the color of index 255 and use alpha as a mask
+    im.paste(255, mask)
+
+    # The transparency index is 255
+    im.info['transparency'] = 255
+
+    return im
