071c4effcc5b4ba2e56b458dd5686676b8f17fa0
Adam Roth <81182782+aroth1338@users.noreply.github.com>
2022-03-31 14:53:26 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     # Needed to actually package something
     packages=['data_visualization'],
     # Needed for dependencies
-    install_requires=['numpy', 'matplotlib'],
+    install_requires=['numpy', 'matplotlib >= 3.5.0'],
     # *strongly* suggested for sharing
     version='0.4.6',
     # The license can be anything you like
