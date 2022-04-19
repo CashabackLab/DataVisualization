a0f97367310ad3168f4046002989a05c735fe993
Adam Roth <81182782+aroth1338@users.noreply.github.com>
2022-04-19 11:39:27 -0400

    Create jitterdata.py

---
+++ data_visualization/jitterdata.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+def jitterdata(ax, left_x, right_x, left_data, right_data, noise_scale, **kwargs):
+    circle_size  = kwargs.get("circle_size", 8)
+    circle_alpha = kwargs.get("circle_alpha", .8)
+    
+    for i in range(len(left_data)):
+        noise = np.random.normal(0, noise_scale)
+
+        ax.plot([left_x + noise, right_x + noise], [left_data[i], right_data[i]],
+                 lw = .3, c = "#B2B1B3", alpha = circle_alpha, zorder = 0)
+
+        ax.scatter([left_x + noise, right_x + noise], [left_data[i], right_data[i]],
+                    s = circle_size, zorder = 0, facecolors = 'none',
+                   edgecolors='grey', alpha = circle_alpha, lw = .5)
