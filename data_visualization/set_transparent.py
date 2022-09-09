94f7e52118614ef910c06eb5ca4d12e9fb4a20d5
Adam Roth <81182782+aroth1338@users.noreply.github.com>
2022-09-09 11:04:48 -0400

    Update and rename transparent_plot.py to set_transparent.py

--- data_visualization/set_transparent.py
+++ data_visualization/set_transparent.py
@@ -1,9 +1,12 @@
 import matplotlib.pyplot as plt
 
-def transparent_plot(ax):
+def set_transparent(ax):
   """
   Removes background patch of plot
   """
   
   ax.get_figure().set_color("none")
+  ax.get_figure().set_alpha(0)
+  
   ax.patch.set_color("none")
+  ax.patch.set_alpha(0)
