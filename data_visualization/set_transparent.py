916937700e67b679fdfb798e693132e79980ea8d
Adam Roth <81182782+aroth1338@users.noreply.github.com>
2022-09-09 11:07:29 -0400

    Update set_transparent.py

--- data_visualization/set_transparent.py
+++ data_visualization/set_transparent.py
@@ -5,8 +5,8 @@ def set_transparent(ax):
   Removes background patch of plot
   """
   
-  ax.get_figure().set_color("none")
-  ax.get_figure().set_alpha(0)
+  ax.get_figure().patch.set_color("none")
+  ax.get_figure().patch.set_alpha(0)
   
   ax.patch.set_color("none")
   ax.patch.set_alpha(0)
