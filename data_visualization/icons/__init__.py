c468c1370ad292ae41558a606a28f1bc2db699a1
Adam Roth <81182782+aroth1338@users.noreply.github.com>
2022-10-20 15:05:58 -0400

    Update __init__.py

--- data_visualization/icons/__init__.py
+++ data_visualization/icons/__init__.py
@@ -1,1 +1,3 @@
-from .get_kinarm import get_kinarm
+from .get_kinarm import get_kinarm as kinarm
+
+from .get_icons import healthy, elderly, hand, open_hand, foot, walking, reward, sight, punishment
