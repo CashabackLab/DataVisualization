06e4554f3f8e764596692d00218ce766db331c08
Seth Sullivan <89952458+SethSullivan@users.noreply.github.com>
2024-02-02 14:27:15 -0500

    Update __init__.py including AutoFigure

--- data_visualization/__init__.py
+++ data_visualization/__init__.py
@@ -9,6 +9,8 @@ from .Pair_Plot import *
 from .Stat_Annotation import *
 from .jitterdata import jitterdata
 from .boxplot import boxplot
+from .AutoFigure import AutoFigure
+
 
 from .generate_icons import get_sight_icon, get_reward_icon, get_punishment_icon, get_hand_icon, get_open_hand_icon, get_foot_icon, get_walking_icon
 from .generate_icons import get_elderly_icon, get_healthy_icon
