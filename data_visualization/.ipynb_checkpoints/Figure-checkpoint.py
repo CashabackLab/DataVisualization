583a31b363379f59770ee633c33cab735af57d62
John Buggeln <john.buggeln@gmail.com>
2025-03-27 15:29:22 -0400

    Added "set_plot_style" to set style sheets

---
+++ data_visualization/.ipynb_checkpoints/Figure-checkpoint.py
@@ -0,0 +1,88 @@
+from string import ascii_uppercase
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+class Figure:
+    def __init__(self, axmain = None, ax = None, figsize = (6.5, 4), dpi = 150, invert = True):
+        
+        if axmain == None and ax == None:
+            fig = plt.figure(dpi = dpi, figsize = figsize)
+            axmain = plt.gca()
+
+            fig.patch.set_alpha(0)
+            axmain.patch.set_alpha(0)
+            axmain.set_position([0,0,1,1])
+
+            axmain.set_ylim(0, figsize[1])
+            axmain.set_xlim(0, figsize[0])
+
+            inset_dimensions = [0, 0, figsize[0], figsize[1]]
+            ax = axmain.inset_axes(inset_dimensions, transform=axmain.transData)
+
+            ax.patch.set_alpha(0)
+            ax.set_ylim(0, inset_dimensions[3])
+            ax.set_xlim(0, inset_dimensions[2])
+
+        self.axmain, self.ax = axmain, ax
+        
+        self.figure = self.axmain.get_figure()
+
+        #Enforces axes on regardless of stylesheet
+        self.axmain.spines.top.set_visible(True)
+        self.axmain.spines.right.set_visible(True)
+        self.axmain.spines.bottom.set_visible(True)
+        self.axmain.spines.left.set_visible(True)
+
+        self.ax.spines.top.set_visible(True)
+        self.ax.spines.right.set_visible(True)
+        self.ax.spines.bottom.set_visible(True)
+        self.ax.spines.left.set_visible(True)
+        
+        self.figsize = figsize
+        self.dpi = dpi
+        
+        if invert:
+            self.axmain.invert_yaxis()
+            self.ax.invert_yaxis()
+        
+        self.letters = [] #letters for annotating figure
+        self.panels  = [] #panels for figure
+        
+    def remove_figure_borders(self):
+        self.axmain.axis("off")
+        self.ax.axis("off")
+        
+    def remove_panel_borders(self):
+        for panel in self.panels:
+            panel.axis("off")
+        
+    def add_letter(self, x, y, letter = None, fontsize = 12, ha = "left", va = "top", color = "black", zorder = 20):
+        if letter == None:
+            letter_to_add = ascii_uppercase[len(self.letters)]
+        else:
+            letter_to_add = letter
+        
+        self.letters.append(letter_to_add)
+        self.ax.text(x, y, letter_to_add, ha = ha, va = va, fontweight = "bold", color = color, fontsize = fontsize, zorder = zorder)
+        
+    def add_panel(self, dim):
+        panel = self.ax.inset_axes(dim, transform = self.ax.transData)
+        self.panels.append(panel)
+        
+        return panel
+    
+    def highlight_panel(self, panel):
+        if isinstance(panel, int):
+            for spine in self.panels[0].spines:
+                self.panels[0].spines[spine].set_color("red")
+                self.panels[0].spines[spine].set_visible(True)
+                
+        elif isinstance(panel, mpl.axes.Axes):
+            panel.axis("on")
+            for spine in panel.spines:
+                panel.spines[spine].set_color("red")
+                panel.spines[spine].set_visible(True)
+                
+    def savefig(self, path, dpi = 300, **kwargs):
+        self.remove_figure_borders()
+        self.figure.savefig(path, dpi = dpi, **kwargs)
---
+++ data_visualization/.ipynb_checkpoints/generate_rectangle_icons-checkpoint.py
@@ -0,0 +1,342 @@
+from matplotlib.patches import Rectangle
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import PIL
+import numpy as np
+from .RotatingRectangle import RotatingRectangle
+
+def get_hidden_rectangle(color, arrow_direction, height = 12, width = .7, wide_scalar = 4, arr_width = 0.2, figsize = (2, 2), edge_color = "#727273",
+                         dim_len = 6.3, **kwargs):
+    """
+    Returns a short rectangle target PIL Image.
+    Plottable using plt.imshow()
+    
+    Kwargs:
+    rect_x = kwargs.get("rect_x", 0)
+    rect_y = kwargs.get("rect_y", -1)
+    square_width = kwargs.get("square_width", .9)
+    square_length = kwargs.get("square_length", 2)
+    
+    linewidth = kwargs.get("linewidth", 2)
+    lw = kwargs.get("lw", linewidth)
+    visible_facecolor = kwargs.get("visible_facecolor", "white")
+    hidden_facecolor = kwargs.get("hidden_facecolor", "none")
+    """
+    rect_x = kwargs.get("rect_x", 0)
+    rect_y = kwargs.get("rect_y", -1)
+    square_width = kwargs.get("square_width", .9)
+    square_length = kwargs.get("square_length", 2)
+    
+    linewidth = kwargs.get("linewidth", 2)
+    lw = kwargs.get("lw", linewidth)
+    
+    visible_facecolor = kwargs.get("visible_facecolor", "white")
+    hidden_facecolor = kwargs.get("hidden_facecolor", "none")
+    
+    fig, ax = plt.subplots(figsize = figsize, dpi = 300, facecolor = "none")
+    ax.set_aspect('equal')
+    ax.set_position([0,0,1,1])
+
+    ax.set_xlim([-dim_len, dim_len])
+    ax.set_ylim([-dim_len, dim_len])
+    ax.patch.set_alpha(0)
+    point_of_rotation = np.array([width/2, height/2])  # B
+
+    #make the arrow
+    arr_color = color
+    edgewidth = 2
+
+    #Make the long target
+    r1 = RotatingRectangle((rect_x, rect_y), width=width, height=height, rel_point_of_rot=point_of_rotation,
+                            angle=45, facecolor=hidden_facecolor, alpha=1,
+                           edgecolor = edge_color, linewidth = lw, linestyle = "--")
+    ax.add_patch(r1)
+    hidden_center = r1.xy_center
+    point_of_rotation = np.array([square_width/2, square_length/2])  # B
+
+    #make the small target
+    r2 = RotatingRectangle(hidden_center, width=square_width, height=square_length, rel_point_of_rot=point_of_rotation,
+                            angle=45, facecolor=visible_facecolor, alpha=1,
+                           edgecolor = edge_color, linewidth = lw)
+    ax.add_patch(r2)
+
+    coords = r1.get_patch_transform().transform(r1.get_path().vertices[:-1])
+        
+    if arrow_direction.lower() == "extent":
+
+        index1 = 2 # top right corner
+        index2 = 1 #bottom right corner
+        arr_shift = width * 1 
+
+        dx, dy = -coords[index2][0] + coords[index1][0], -coords[index2][1] + coords[index1][1]
+        plt.arrow(coords[index2][0]+ arr_shift, coords[index2][1]+ arr_shift, dx, dy,
+                   facecolor = arr_color, edgecolor = arr_color, width = arr_width)
+        plt.arrow(coords[index1][0]+ arr_shift, coords[index1][1]+ arr_shift, -dx, -dy,
+                   facecolor = arr_color, edgecolor = arr_color, width = arr_width)
+
+    if arrow_direction.lower() == "aim":
+    #make the arrow
+        index1 = 2 # top right corner
+        index2 = 3 #bottom right corner
+        arr_shift = width  * 1
+
+        dx, dy = -coords[index2][0] + coords[index1][0], -coords[index2][1] + coords[index1][1]
+        plt.arrow(coords[index2][0] - arr_shift, coords[index2][1]+ arr_shift, dx, dy,
+                   facecolor = arr_color, edgecolor = arr_color, width = arr_width)
+        plt.arrow(coords[index1][0] - arr_shift, coords[index1][1]+ arr_shift, -dx, -dy,
+                   facecolor = arr_color, edgecolor = arr_color, width = arr_width)
+
+    plt.axis("off")
+    
+    fig.canvas.draw() #draws the figure to create the renderer object
+    
+    #convert to Image object
+    img = PIL.Image.frombytes('RGBA', fig.canvas.get_width_height(),np.asarray(fig.canvas.buffer_rgba()).take([0, 1, 2, 3], axis=2).tobytes())
+    plt.close()
+    
+    return img
+  
+def get_hidden_wide_rectangle(color, arrow_direction, height = 12, width = 1.04, wide_scalar = 4, arr_width = 0.2,
+                         figsize = (2, 2), edge_color = "#727273", dim_len = 8, **kwargs):
+    """
+    Returns a short rectangle target PIL Image.
+    Plottable using plt.imshow()
+    
+    Kwargs:
+    rect_x = kwargs.get("rect_x", 0)
+    rect_y = kwargs.get("rect_y", -1)
+    remove_reward_zone = kwargs.get("remove_reward_zone", False)
+    
+    linewidth = kwargs.get("linewidth", 2)
+    lw = kwargs.get("lw", linewidth)
+    visible_facecolor = kwargs.get("visible_facecolor", "white")
+    hidden_facecolor = kwargs.get("hidden_facecolor", "none")
+    """
+    rect_x = kwargs.get("rect_x", 0)
+    rect_y = kwargs.get("rect_y", -1)
+    remove_reward_zone = kwargs.get("remove_reward_zone", False)
+
+    linewidth = kwargs.get("linewidth", 2)
+    lw = kwargs.get("lw", linewidth)
+    
+    visible_facecolor = kwargs.get("visible_facecolor", "white")
+    hidden_facecolor = kwargs.get("hidden_facecolor", "none")
+    
+    fig, ax = plt.subplots(figsize = figsize, dpi = 300, facecolor = "none")
+    ax.set_aspect('equal')
+    ax.set_position([0,0,1,1])
+
+    ax.set_xlim([-dim_len, dim_len])
+    ax.set_ylim([-dim_len, dim_len])
+    ax.patch.set_alpha(0)
+    point_of_rotation = np.array([width/2*wide_scalar, height/2])  # B
+
+    #make the arrow
+    arr_color = color
+
+    #Make the Wide target
+    r1 = RotatingRectangle((rect_x, rect_y), width=width*wide_scalar, height=height, rel_point_of_rot=point_of_rotation,
+                            angle=45, facecolor=hidden_facecolor, alpha=1,
+                           edgecolor = edge_color, linewidth = lw, linestyle = "--")
+    
+    if not remove_reward_zone:
+        ax.add_patch(r1)
+
+    point_of_rotation = np.array([width/2, height/2])  # B
+
+    #make the Normal target
+    r2 = RotatingRectangle((rect_x, rect_y), width=width, height=height, rel_point_of_rot=point_of_rotation,
+                            angle=45, facecolor=visible_facecolor, alpha=1,
+                           edgecolor = edge_color, linewidth = lw)
+    ax.add_patch(r2)
+
+    coords = r1.get_patch_transform().transform(r1.get_path().vertices[:-1])
+        
+    if arrow_direction.lower() == "extent":
+
+        index1 = 2 # top right corner
+        index2 = 1 #bottom right corner
+        arr_shift = width * 1 
+
+        dx, dy = -coords[index2][0] + coords[index1][0], -coords[index2][1] + coords[index1][1]
+        plt.arrow(coords[index2][0]+ arr_shift, coords[index2][1]+ arr_shift, dx, dy,
+                   facecolor = arr_color, edgecolor = arr_color, width = arr_width)
+        plt.arrow(coords[index1][0]+ arr_shift, coords[index1][1]+ arr_shift, -dx, -dy,
+                   facecolor = arr_color, edgecolor = arr_color, width = arr_width)
+
+    if arrow_direction.lower() == "aim":
+    #make the arrow
+        index1 = 2 # top right corner
+        index2 = 3 #bottom right corner
+        arr_shift = width  * 1
+
+        dx, dy = -coords[index2][0] + coords[index1][0], -coords[index2][1] + coords[index1][1]
+        plt.arrow(coords[index2][0] - arr_shift, coords[index2][1]+ arr_shift, dx, dy,
+                   facecolor = arr_color, edgecolor = arr_color, width = arr_width)
+        plt.arrow(coords[index1][0] - arr_shift, coords[index1][1]+ arr_shift, -dx, -dy,
+                   facecolor = arr_color, edgecolor = arr_color, width = arr_width)
+
+    plt.axis("off")
+    
+    fig.canvas.draw() #draws the figure to create the renderer object
+    
+    #convert to Image object
+    img = PIL.Image.frombytes('RGBA', fig.canvas.get_width_height(),np.asarray(fig.canvas.buffer_rgba()).take([0, 1, 2, 3], axis=2).tobytes())
+    plt.close()
+    
+    return img
+  
+def get_long_rectangle(color, arrow_direction, height = 12, width = 0.7, arr_width = 0.2, figsize = (2, 2),
+                       edge_color = "#727273", dim_len = 6.3, **kwargs):
+    """
+    Returns a long rectangle target PIL Image.
+    Plottable using plt.imshow()
+    
+    Kwargs:
+    rect_x = kwargs.get("rect_x", 0)
+    rect_y = kwargs.get("rect_y", -1)
+    linewidth = kwargs.get("linewidth", 2)
+    lw = kwargs.get("lw", linewidth)
+    
+    visible_facecolor = kwargs.get("visible_facecolor", "white")    
+    """
+    
+    rect_x = kwargs.get("rect_x", 0)
+    rect_y = kwargs.get("rect_y", -1)
+    
+    linewidth = kwargs.get("linewidth", 2)
+    lw = kwargs.get("lw", linewidth)
+    visible_facecolor = kwargs.get("visible_facecolor", "white")
+
+    fig, ax = plt.subplots(figsize = figsize, dpi = 300, facecolor = "none")
+    ax.set_aspect('equal')
+    ax.set_position([0,0,1,1])
+
+    ax.set_xlim([-dim_len, dim_len])
+    ax.set_ylim([-dim_len, dim_len])
+    ax.patch.set_alpha(0)
+    point_of_rotation = np.array([width/2, height/2])  # B
+
+    #Make the target
+    r1 = RotatingRectangle((rect_x, rect_y), width=width, height=height, rel_point_of_rot=point_of_rotation,
+                            angle=45, facecolor=visible_facecolor, alpha=1,
+                           edgecolor = edge_color, linewidth = lw)
+    ax.add_patch(r1)
+    coords = r1.get_patch_transform().transform(r1.get_path().vertices[:-1])
+
+    #make the arrow
+    arr_color = color
+    
+    if arrow_direction.lower() == "extent":
+        index1 = 2 # top right corner
+        index2 = 1 #bottom right corner
+        arr_shift = width * .75
+
+        dx, dy = -coords[index2][0] + coords[index1][0], -coords[index2][1] + coords[index1][1]
+        plt.arrow(coords[index2][0]+ arr_shift, coords[index2][1]+ arr_shift, dx, dy,
+                   facecolor = arr_color, edgecolor = arr_color, width = arr_width)
+        plt.arrow(coords[index1][0]+ arr_shift, coords[index1][1]+ arr_shift, -dx, -dy,
+                   facecolor = arr_color, edgecolor = arr_color, width = arr_width)
+
+    elif arrow_direction.lower() == "aim":
+        index1 = 2 # top right corner
+        index2 = 3 #bottom right corner
+        
+        arr_shift = width * .75
+
+        dx, dy = -coords[index2][0] + coords[index1][0], -coords[index2][1] + coords[index1][1]
+        plt.arrow(coords[index2][0] - arr_shift, coords[index2][1]+ arr_shift, dx, dy,
+                   facecolor = arr_color, edgecolor = arr_color, width = arr_width)
+        plt.arrow(coords[index1][0]- arr_shift, coords[index1][1]+ arr_shift, -dx, -dy,
+                   facecolor = arr_color, edgecolor = arr_color, width = arr_width)
+        
+    else: 
+        pass
+
+    plt.axis("off")
+    
+    fig.canvas.draw() #draws the figure to create the renderer object
+    
+    #convert to Image object
+    img = PIL.Image.frombytes('RGBA', fig.canvas.get_width_height(),np.asarray(fig.canvas.buffer_rgba()).take([0, 1, 2, 3], axis=2).tobytes())
+    plt.close()
+    
+    return img
+  
+def get_short_rectangle(color, arrow_direction, height = 2, width = .9, arr_width = 0.2, figsize = (2, 2),
+                        edge_color = "#727273", dim_len = 6.3, **kwargs):
+    """
+    Returns a short rectangle target PIL Image.
+    Plottable using plt.imshow()
+    
+    Kwargs:
+    rect_x = kwargs.get("rect_x", 0)
+    rect_y = kwargs.get("rect_y", -1)
+    
+    linewidth = kwargs.get("linewidth", 2)
+    lw = kwargs.get("lw", linewidth)
+    
+    visible_facecolor = kwargs.get("visible_facecolor", "white")
+    
+    """
+    rect_x = kwargs.get("rect_x", 0)
+    rect_y = kwargs.get("rect_y", -1)
+    
+    linewidth = kwargs.get("linewidth", 2)
+    lw = kwargs.get("lw", linewidth)
+    visible_facecolor = kwargs.get("visible_facecolor", "white")
+
+    fig, ax = plt.subplots(figsize = figsize, dpi = 300, facecolor = "none")
+    ax.set_aspect('equal')
+    ax.set_position([0,0,1,1])
+
+    ax.set_xlim([-dim_len, dim_len])
+    ax.set_ylim([-dim_len, dim_len])
+    ax.patch.set_alpha(0)
+    point_of_rotation = np.array([width/2, height/2])  # B
+
+    #Make the target
+    r1 = RotatingRectangle((rect_x, rect_y), width=width, height=height, rel_point_of_rot=point_of_rotation,
+                            angle=45, facecolor=visible_facecolor, alpha=1,
+                           edgecolor = edge_color, linewidth = lw)
+    ax.add_patch(r1)
+    coords = r1.get_patch_transform().transform(r1.get_path().vertices[:-1])
+
+    #make the arrow
+    arr_color = color
+    
+    if arrow_direction.lower() == "extent":
+        index1 = 2 # top right corner
+        index2 = 1 #bottom right corner
+        arr_shift = width * .75
+
+        dx, dy = -coords[index2][0] + coords[index1][0], -coords[index2][1] + coords[index1][1]
+        plt.arrow(coords[index2][0]+ arr_shift, coords[index2][1]+ arr_shift, dx, dy,
+                   facecolor = arr_color, edgecolor = arr_color, width = arr_width)
+        plt.arrow(coords[index1][0]+ arr_shift, coords[index1][1]+ arr_shift, -dx, -dy,
+                   facecolor = arr_color, edgecolor = arr_color, width = arr_width)
+
+    elif arrow_direction.lower() == "aim":
+        index1 = 2 # top right corner
+        index2 = 3 #bottom right corner
+        
+        arr_shift = width * .75
+
+        dx, dy = -coords[index2][0] + coords[index1][0], -coords[index2][1] + coords[index1][1]
+        plt.arrow(coords[index2][0] - arr_shift, coords[index2][1]+ arr_shift, dx, dy,
+                   facecolor = arr_color, edgecolor = arr_color, width = arr_width)
+        plt.arrow(coords[index1][0]- arr_shift, coords[index1][1]+ arr_shift, -dx, -dy,
+                   facecolor = arr_color, edgecolor = arr_color, width = arr_width)
+
+    else:
+        pass
+    
+    plt.axis("off")
+    
+    fig.canvas.draw() #draws the figure to create the renderer object
+    
+    #convert to Image object
+    img = PIL.Image.frombytes('RGBA', fig.canvas.get_width_height(),np.asarray(fig.canvas.buffer_rgba()).take([0, 1, 2, 3], axis=2).tobytes())
+    plt.close()
+    
+    return img
