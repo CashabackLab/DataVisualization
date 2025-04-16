26fa26d8b4f70bfd72fd7334659f7405ddefa00a
John Buggeln <john.buggeln@gmail.com>
2025-04-16 13:00:07 -0400

    Added a curly brace to icons

--- data_visualization/generate_icons.py
+++ data_visualization/generate_icons.py
@@ -198,6 +198,34 @@ def get_sight_icon(color = "#000000"):
     img.putdata(newData)
     return img
 
+def get_curlybrace_icon(color = "#000000"):
+    """
+    Color must be in Hex Code
+    """
+    
+    fileName = "CurlyBrace"
+    pic_format = "png"
+    new_color = color #change this to desired color
+
+    path = os.path.dirname(os.path.realpath(__file__)) # getting full parent directory (no wildcards) of current module (file)
+    filepath = os.path.join(path, "images", fileName)
+    img = Image.open(filepath + '.' + pic_format)
+    img = img.convert("RGBA")
+    datas = img.getdata()
+
+    new_color = hex_to_rgb(new_color)
+
+    newData = []
+
+    for i, item in enumerate(datas):
+        if item[0] >= 245 and item[1] >= 245 and item[2] >= 245:
+            newData.append((255, 255, 255, 0))
+        else:
+            newData.append((new_color[0], new_color[1], new_color[2], 255))
+
+    img.putdata(newData)
+    return img
+
 def get_reward_icon(color = "#000000"):
     """
     Returns a Reward Sound Icon in the given color.
@@ -263,3 +291,5 @@ def get_punishment_icon(color = "#000000"):
 
     img.putdata(newData)
     return img
+
+
