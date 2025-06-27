cdee88b24ad7b13a1c3b0e961fa599ed58b46641
JanCalalo <97128826+JanCalalo@users.noreply.github.com>
2025-06-27 10:28:18 -0400

    Update legend.py

--- data_visualization/legend.py
+++ data_visualization/legend.py
@@ -15,10 +15,12 @@ def legend(ax, labels, colors, ncol = 1,
     
     Returns a legend object
     """
-    
-    if len(labels) != len(colors):
-        raise RuntimeError("Number of Labels should match number of Colors.")
         
+    legend_count = len(labels)
+                        
+    if len(colors) != legend_count:
+        raise RuntimeError("Number of Labels should match number of Colors.")
+    
     if lw == None and linewidth == None:
         linewidth = 4
     elif lw != None and linewidth == None:
@@ -30,20 +32,82 @@ def legend(ax, labels, colors, ncol = 1,
         linestyle = ls
         
     if not isinstance(linestyle,(list, np.ndarray)):
-        linestyle = [linestyle]*len(colors)        
+        linestyle = [linestyle]*legend_count
     
+    if not isinstance(handlestyle,(list,np.ndarray)):
+        handlestyle = [handlestyle]*legend_count
+    
+    if len(handlestyle) != legend_count:
+        raise RuntimeError("Number of HandleStyles should match number of Labels")
+        
+    if not isinstance(markersize,(list,np.ndarray)):
+        markersize = [markersize]*legend_count
+
+    if len(markersize) != legend_count:
+        raise RuntimeError("Number of markersizes should match number of Labels")
+                
+    print(markersize)
     custom_handles = []            
-    if handlestyle == "bar":
-        for i,color in enumerate(colors):
-            custom_handles.append(Line2D([0], [0], color=color, 
-                                         lw=linewidth, ls = linestyle[i]))
-    elif handlestyle == "circle":
-        if handletextpad is None:
-            handletextpad = 0
-        for i,color in enumerate(colors):
-            custom_handles.append(Line2D([], [], marker='o', markersize=markersize, 
-                                         markerfacecolor=color, markeredgecolor=color, 
-                                         ls="none", lw=linewidth))
+    
+    for i in range(legend_count):
+        
+        
+        curr_handle = handlestyle[i]
+        curr_color = colors[i]
+        curr_linestyle = linestyle[i]
+        curr_markersize = markersize[i]
+        
+        
+        if curr_handle == "bar":
+            marker_style = None
+            curr_markersize = None
+            curr_linestyle = linestyle[i]
+
+            curr_facecolor = None
+            curr_edgecolor = None
+
+        elif curr_handle == "o":
+            marker_style = "o"
+            if handletextpad is None:
+                handletextpad = 0
+            curr_linestyle = "none"
+            
+            curr_facecolor = curr_color
+            curr_edgecolor = curr_color
+            
+        elif curr_handle == "circle":
+            marker_style = "o"
+            if handletextpad is None:
+                handletextpad = 0
+            curr_linestyle = "none"
+
+            curr_facecolor = curr_color
+            curr_edgecolor = curr_color
+
+        elif curr_handle == "x":
+            marker_style = "x"
+            if handletextpad is None:
+                handletextpad = 0
+            curr_linestyle = "none"
+
+            curr_facecolor = curr_color
+            curr_edgecolor = curr_color
+            
+        elif  curr_handle == "hollow circle":
+            
+            marker_style = "o"
+            if handletextpad is None:
+                handletextpad = 0
+            curr_linestyle = "none"
+            
+            curr_facecolor = "none"
+            curr_edgecolor = curr_color
+
+        custom_handles.append(Line2D([], [], marker=marker_style, markersize=curr_markersize, 
+                                     markerfacecolor=curr_facecolor, markeredgecolor=curr_edgecolor, 
+                                     ls=curr_linestyle, lw=linewidth))
+        
+        
             
     leg = ax.legend(custom_handles, labels, fontsize = fontsize,
              framealpha = framealpha, loc = loc, ncol = ncol, 
