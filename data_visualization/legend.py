9cbf1df494212a294ba10f06f62956ceaf741649
SethSullivan <sethsull@udel.edu>
2025-08-15 14:02:15 -0400

    get rid of handletextpad defaulting to 0 if None... default of None works

--- data_visualization/legend.py
+++ data_visualization/legend.py
@@ -61,9 +61,6 @@ def legend(
     if len(markersize) != legend_count:
         raise RuntimeError("Number of markersizes should match number of Labels")
 
-    if handletextpad is None:
-        handletextpad = 0
-
     custom_handles = []
 
     for i in range(legend_count):
@@ -74,8 +71,6 @@ def legend(
         curr_markersize = markersize[i]
 
         if curr_handle in  ["bar","b"] :
-            # Increase handletextpad if a bar is used
-            handletextpad = 0.8
             marker_style = None
             curr_markersize = None
             curr_linestyle = linestyle[i]
@@ -120,7 +115,7 @@ def legend(
                 lw=linewidth,
             )
         )
-
+    print(handletextpad)
     leg = ax.legend(
         custom_handles,
         labels,
